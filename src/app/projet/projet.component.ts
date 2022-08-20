9d7d974fc3cce5b6b2487035c3eff6a2631a7f5e
CyrineALOUI <cyrine.aloui@esprit.tn>
2022-08-20 11:23:30 +0200

    first commit

---
+++ src/app/projet/projet.component.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projet',
+  templateUrl: './projet.component.html',
+  styleUrls: ['./projet.component.scss']
+})
+export class ProjetComponent implements OnInit {
+
+  projet:any=[];
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+    
+  }
+
+}
